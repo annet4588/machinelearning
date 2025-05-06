162c1b877473738d46ac227f333d4fdc86dc265b
annet4588 <67749793+annet4588@users.noreply.github.com>
2025-05-06 10:34:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ st.info('This is a machine learning app')
 
 with st.expander('Data'):
   st.write('**Raw Data**')
-  df = pd.read_csv('https://github.com/annet4588/machinelearning/blob/master/penguins.csv')
+  df = pd.read_csv('https://raw.githubusercontent.com/annet4588/machinelearning/blob/master/penguins.csv')
