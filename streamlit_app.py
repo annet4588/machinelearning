42297c1d9df92ceb4b77828c237a215a0f9985a0
annet4588 <67749793+annet4588@users.noreply.github.com>
2025-05-06 10:28:05 +0100

    Read data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning App')
 
 st.info('This is a machine learning app')
+
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('penguins.csv')
