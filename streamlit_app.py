0b1aa08d8100e4d17cc9bd015c351243edf90f93
annet4588 <67749793+annet4588@users.noreply.github.com>
2025-05-06 10:43:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import os
 
 st.title('🤖 Machine Learning App')
 
@@ -8,3 +9,7 @@ st.info('This is a machine learning app')
 with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv('penguins.csv')
+
+
+st.write('Current working directory:', os.getcwd())
+st.write('Files in directory:', os.listdir())
