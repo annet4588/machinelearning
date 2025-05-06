8bda745f7a2eb0f92f31f2b172df8de85279eca5
annet4588 <67749793+annet4588@users.noreply.github.com>
2025-05-06 11:38:08 +0100

    Encode

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ data = {'island': island,
         'bill_depth_mm': bill_depth_mm,
         'flipper_length_mm': flipper_length_mm,
         'body_mass_g': body_mass_g,
-        'gender': gender}
+        'sex': gender}
 
 input_df = pd.DataFrame(data, index=[0])
 input_penguins = pd.concat([input_df, X], axis=0)
@@ -62,4 +62,7 @@ with st.expander('Input Features'):
   st.write('**Combine Penguins Data**')
   input_penguins
 
-
+# Encode
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+df_penguins[:1]
